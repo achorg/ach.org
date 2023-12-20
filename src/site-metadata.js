c4a521a5166ae03d3daae52c36d12be38e3773ce
Simon Wiles <simonjwiles@gmail.com>
2023-12-20 12:29:14 +0800

    Add initial site metadata

---
+++ src/site-metadata.js
@@ -0,0 +1,68 @@
+module.exports = {
+  title: `The Association for Computers and the Humanities`,
+  description: `The Association for Computers and the Humanities (ACH) is a major professional society for the digital humanities. We support and disseminate research and cultivate a vibrant professional community through conferences, publications, and outreach activities.`,
+  menuLinks: [
+    {
+      name: "About",
+      link: "/about/",
+      subMenu: [
+        {
+          name: "Constitution & Bylaws",
+          link: "/about/constitution/",
+        },
+        {
+          name: "Elections",
+          link: "/about/elections/",
+        },
+        {
+          name: "History",
+          link: "/about/history/",
+        },
+        {
+          name: "Officers and Council Members",
+          link: "/about/officers/",
+        },
+        {
+          name: "Alumni",
+          link: "/about/alumni/",
+        },
+      ],
+    },
+    {
+      name: "Activities",
+      link: "/activities",
+      subMenu: [
+        {
+          name: "Advocacy",
+          link: "/activities/advocacy/",
+        },
+        {
+          name: "Grants and Awards",
+          link: "/activities/grants-and-awards/",
+        },
+        {
+          name: "Professional Development",
+          link: "/activities/mentoring/",
+        },
+        {
+          name: "Publications",
+          link: "/publications/",
+        },
+      ],
+    },
+    {
+      name: "Conferences",
+      link: "/conferences",
+    },
+    {
+      name: "News",
+      link: "/news/",
+    },
+
+    {
+      name: "Join ACH",
+      link: "https://members.ach.org/",
+    },
+  ],
+  siteUrl: `https://ach.org/`,
+};
