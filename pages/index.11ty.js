ce7bd9b3ff0247871a2dd4c08276bfcbe67b7a36
Simon Wiles <simonjwiles@gmail.com>
2023-12-20 15:04:59 +0800

    Add the Call-To-Action section

--- pages/index.11ty.js
+++ pages/index.11ty.js
@@ -1,11 +1,12 @@
 const Hero = require("../src/components/Hero.js");
+const CallToAction = require("../src/components/CallToAction.js");
 
 module.exports = {
   data: {
     layout: "layouts/base.11ty.js",
     title: "Home",
   },
-  render(data) {
+  async render(data) {
     return `
         <section class="boxed-fullwidth">
           ${Hero({
@@ -19,7 +20,22 @@ module.exports = {
           -- RECENT POSTS --
         </section>
         <section class="boxed-fullwidth">
-          -- CALL TO ACTION --
+
+          ${await CallToAction({
+            title: "Solidarity with BLM",
+            body: "The Association for Computers and the Humanities stands in solidarity with the Black Lives Matter protest movement. See our statement on Black Lives Matter, Structural Racism, and Our Organization.",
+            imageSrc: "./src/images/black-lives-matter.jpg",
+            imageAlt:
+              "Hands holding up various Black Lives Matter protest signs",
+            link: `
+              <a
+                href="/news/2020/06/ach-statement-on-black-lives-matter-structural-racism-and-our-organization/"
+                class="button-pill button-accent"
+              >
+                Read more
+              </a>
+            `,
+          })}
         </section>    
     `;
   },
---
+++ src/components/CallToAction.js
@@ -0,0 +1,35 @@
+const Image = require("@11ty/eleventy-img");
+
+const image = async (src, alt) => {
+  let metadata = await Image(src, {
+    formats: ["avif", "webp", "jpeg"],
+    widths: [600, 1200, 2000, "auto"],
+    outputDir: "./_site/img/",
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes:
+      "(max-width: 600px) 600px, (max-width: 1200px) 1200px, (max-width: 2000px) 2000px, 100vw",
+    loading: "lazy",
+    decoding: "async",
+    style: "height:100%;width:100%;object-fit:cover",
+  };
+  return Image.generateHTML(metadata, imageAttributes);
+};
+
+module.exports = async ({ title, body, imageSrc, imageAlt, link }) => {
+  return `
+    <div class="call-to-action">
+      <div class="call-to-action-overlay">
+        <div class="call-to-action-content boxed-regular">
+          <h2>${title}</h2>
+          <p>${body}</p>
+          ${link}
+        </div>
+      </div>
+      <div class="call-to-action-image">${await image(imageSrc, imageAlt)}
+      </div>
+    </div>
+  `;
+};
