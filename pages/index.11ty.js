b4838bdf19bc00fac811ea4f0b7033ebf44eeff3
Amanda Regan <aeregan@clemson.edu>
2024-09-02 09:16:02 -0400

    show most recent 2 news posts on homepage

--- pages/index.11ty.js
+++ pages/index.11ty.js
@@ -12,36 +12,38 @@ module.exports = {
     usingHero: true,
   },
   async render(data) {
-    return `
-        <section class="boxed-fullwidth">
-          ${Hero({
-            title: "Amplifying digital humanities.",
-            subtitle:
-              "Our most important activities center on cultivating and strengthening the field of digital humanities, and providing guidance and support to those entering the field.",
-          })}
-        </section>
-        <section class="boxed-regular">
-          <h2 class="section-title">The latest from ACH</h2>
-         
-        </section>
-        <section class="boxed-fullwidth">
+    const recentPosts = data.collections.news.slice(-2);
 
-          ${await CallToAction({
-            title: "Solidarity with BLM",
-            body: "The Association for Computers and the Humanities stands in solidarity with the Black Lives Matter protest movement. See our statement on Black Lives Matter, Structural Racism, and Our Organization.",
-            imageSrc: "./src/images/black-lives-matter.jpg",
-            imageAlt:
-              "Hands holding up various Black Lives Matter protest signs",
-            link: `
-              <a
-                href="/news/2020/06/ach-statement-on-black-lives-matter-structural-racism-and-our-organization/"
-                class="button-pill button-accent"
-              >
-                Read more
-              </a>
-            `,
-          })}
-        </section>    
+    return `
+      <section class="boxed-fullwidth">
+      ${Hero({
+      title: "Amplifying digital humanities.",
+      subtitle:
+        "Our most important activities center on cultivating and strengthening the field of digital humanities, and providing guidance and support to those entering the field.",
+      })}
+      </section>
+      <section class="boxed-regular">
+      <h2 class="section-title">The latest from ACH</h2>
+      ${NewsItems(recentPosts, "h3", "list")}
+      </section>
+      <section class="boxed-fullwidth">
+
+      ${await CallToAction({
+      title: "Solidarity with BLM",
+      body: "The Association for Computers and the Humanities stands in solidarity with the Black Lives Matter protest movement. See our statement on Black Lives Matter, Structural Racism, and Our Organization.",
+      imageSrc: "./src/images/black-lives-matter.jpg",
+      imageAlt:
+        "Hands holding up various Black Lives Matter protest signs",
+      link: `
+        <a
+        href="/news/2020/06/ach-statement-on-black-lives-matter-structural-racism-and-our-organization/"
+        class="button-pill button-accent"
+        >
+        Read more
+        </a>
+      `,
+      })}
+      </section>    
     `;
   },
 };
